377d862bb2512155bccf0d7f53c0730835773f21
PHANIBHUSHANRAO <phanibhushanrao12@gmail.com>
2021-10-24 13:15:54 +0530

    added testdemotwo file

---
+++ src/test/java/com/test/sample/DemoTwo.java
@@ -0,0 +1,15 @@
+package com.test.sample;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+	@Test
+	public static void test() {
+		System.out.println("DEMOTWO:WELCOME TO RAMESHSSOFT");
+		System.out.println("DEMOTWO:WELCOME TO RAMESHSSOFT");
+		System.out.println("DEMOTWO:WELCOME TO RAMESHSSOFT");
+		System.out.println("DEMOTWO:WELCOME TO RAMESHSSOFT");
+		
+		
+	}
+}
