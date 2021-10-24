6434a00cc970fcf266b660847d3e821d79c25bdd
PHANIBHUSHANRAO <phanibhushanrao12@gmail.com>
2021-10-24 11:43:37 +0530

    Added a TestDemoTwo project

---
+++ src/test/java/com/test/sample/DemoOne.java
@@ -0,0 +1,16 @@
+package com.test.sample;
+
+import org.testng.annotations.Test;
+
+public class DemoOne {
+	@Test
+public static void demo() {
+	System.out.println("WELCOME TO RAMESHSSOFT");
+	System.out.println("WELCOME TO RAMESHSSOFT");
+	System.out.println("WELCOME TO RAMESHSSOFT");
+	System.out.println("WELCOME TO RAMESHSSOFT");
+	
+	
+}
+	
+}
